af2f48c0559113b17bc79f14881a7b89bb21341c
TiagoGirao <tiagogirao73@gmail.com>
2024-05-18 15:59:52 -0300

    INICIAL

---
+++ .vscode/script.js
@@ -0,0 +1,25 @@
+function toggleMode() {
+  const html = document.documentElement // Criando uma variável
+  html.classList.toggle("light")
+  //subsituir a foto quando for o light mode
+  // pegar a tag img
+  const img=document.querySelector("#profile img")
+  //substituir as imagens
+
+   if(html.classList.contains("light")) {
+    // se tiver, trocar a iamgem
+    img.setAttribute('src','./assets/Avatar_light_mode.png')
+  }
+  else{
+    // 
+    img.setAttribute('src','./assets/Avatar_tiago.png')
+
+  }
+}
+
+
+
+  
+
+
+
